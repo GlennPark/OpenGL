402aebaa3aaaffcaa2540529812c86940ca4f004
Park Byeong Gyu <42878943+GlennPark@users.noreply.github.com>
2023-01-03 10:46:38 +0900

    0103_02
    Practice02

---
+++ 0103/cudathread.cu
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define NUM_BLOCKS 2
+#define NUM_THREAD 1
+
+__global__ void kernel()
+{
+	printf("Hello Cuda!(%d Thread in %d Block) from Device\n", threadIdx.x, blockIdx.x);
+}
+int main(int argc, char **argv)
+{
+	kernel<<<NUM_BLOCKS, NUM_THREAD>>>();
+
+	cudaDeviceSynchronize();
+	printf("Hello Cuda! from Host\n");
+
+	return 0;
+}
